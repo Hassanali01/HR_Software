8acd6cb9a8017e458de6ed66895af742a8d17e09
AsadIsmail12 <asad.ismail@sagacious.systems>
2023-08-29 10:24:46 +0500

    add functinality that delete slab of shift

--- server/controllers/shiftsController.js
+++ server/controllers/shiftsController.js
@@ -4,9 +4,9 @@ const Shifts = require('../Models/shifts')
 
 //for create shifts
 const addShifts = async (req, res) => {
-    const { shift_name, description, start_time, end_time, slabs,early_leave_slabs } = req.body
+    const { shift_name, description, start_time, end_time, slabs, early_leave_slabs } = req.body
     try {
-        const shifts = await Shifts.create({ shift_name, description, start_time, end_time, slabs,early_leave_slabs })
+        const shifts = await Shifts.create({ shift_name, description, start_time, end_time, slabs, early_leave_slabs })
         res.status(200).json(shifts)
     } catch (error) {
         res.status(400).json({ error: error.message })
@@ -35,7 +35,7 @@ const getSpecificShift = async (req, res) => {
     try {
         const shifts = await Shifts.findById({ _id: id })
         res.status(200).json(shifts)
-      
+
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
@@ -65,14 +65,14 @@ const updateShift = async (req, res) => {
         if (req.body.slabs && req.body.slabs.length > 0) {
             updatedFields.slabs = {
                 later_than: req.body.slabs[0].later_than,
-                deduction: req.body.slabs[0].deduction,
+                deduction: req.body.slabs[1].deduction,
             };
         }
 
         if (req.body.early_leave_slabs && req.body.early_leave_slabs.length > 0) {
             updatedFields.early_leave_slabs = {
                 early_leave_time: req.body.early_leave_slabs[0].early_leave_time,
-                deduction: req.body.early_leave_slabs[0].deduction,
+                deduction: req.body.early_leave_slabs[1].deduction,
             };
         }
 
@@ -89,8 +89,32 @@ const updateShift = async (req, res) => {
     }
 };
 
+// single slab delete for late arrival and early leaves
+const deleteSlabs = async (req, res) => {
+    try {
+        const shiftId = req.params.id;
+        const shift = await Shifts.findById(shiftId);
+
+        if (!shift) {
+            return res.status(404).json({ error: 'Shift not found' });
+        }
+
+        if (req.body.slabs) {
+            shift.slabs = shift.slabs.filter(slab => !req.body.slabs.includes(slab.later_than));
+        }
+        console.log(req.body.early_leave_slabs)
+        if (req.body.early_leave_slabs) {
+            shift.early_leave_slabs = shift.early_leave_slabs.filter(slab => !req.body.early_leave_slabs.includes(slab.early_leave_time));
+        }
+
+        await shift.save();
+
+        return res.status(200).json({ message: 'Slabs deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred while deleting slabs' });
+    }
+};
 
 
-
-
-module.exports = { addShifts, getSpecificShift, allShifts, deleteShifts, updateShift }
+module.exports = { addShifts, getSpecificShift, allShifts, deleteShifts, updateShift, deleteSlabs }
